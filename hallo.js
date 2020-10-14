ccf1166adc29bae82cef83163f0cc982d424c8a9
muhanadzain <48648883+muhanadzain@users.noreply.github.com>
2020-10-14 15:19:12 +0200

    Add files via upload

---
+++ hallo.js
@@ -0,0 +1,20 @@
+
+        let str1 = "Hello World"
+    console.log("str1 => ", str1.toUpperCase())
+
+      let str2 = "Hello Earthling"
+    console.log("str2 => ", str2.toLowerCase())
+ 
+    let str3 = "Bananas"
+    console.log("str3 => ", str3.charAt(0)+ str3 + str3.charAt(0))
+  
+    let str4 = "Memo"
+    console.log("str4 => ", str4.slice(7, 10)+ str4 + str4.slice(7, 10))
+    
+let var5 = "BoogieWoogie";
+console.log(
+  "5==>",
+  var5.slice(0, 11).replace(var5.charAt(0), var5.charAt(11)) + var5[0]);
+
+let var6 = "the quick brown fox";
+console.log("6==>", var6.charAt(0).toUpperCase() + var6.slice(1));

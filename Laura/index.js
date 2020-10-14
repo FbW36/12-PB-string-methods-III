e83331f298a7897ad1ba5b16b62ee9595cb21db5
limononoil <63930000+limononoil@users.noreply.github.com>
2020-10-14 16:37:42 +0200

    difficult exercise

---
+++ Laura/index.js
@@ -0,0 +1,34 @@
+// 13-Programming Basics: string methods
+
+// 1. Declare a variable with the value of "Hello World". Covert the value to upper case and print the converted value to the console.
+let str = "Hello World";
+console.log(str.toUpperCase()); // HELLO WORLD
+
+// 2. Declare another variable with the value of "Hello Earthling". Convert the value to lower case and print the converted value to the console.
+let str2 = "Hello Earthling";
+console.log(str2.toLowerCase()); // hello earthling
+
+// 3. Create a new string from a given string with the first character of the given string added at the front and back, i.e., expected output: Bananas => BBananasB.
+let str3 = "Bananas";
+// let str4 = str3[0] + str3 + str3[0];
+// console.log(str4);
+// let str4 = str3[0].concat(str3).concat(str3[0]);
+// console.log(str4);
+console.log(
+  str3
+    .replace(str3[0], str3[0].concat(str3[0]))
+    .replace(str3[str3.length - 1], str3[str3.length - 1].concat(str3[0]))
+);
+
+// 4. Create a new string from a given string taking the last 3 characters of the string and add them to both the front and back of the new string. The string length must be 3 or more, i.e., expected output: Scritch => tchScritchtch.
+let str4 = "Scritch";
+console.log(str4.substring(4) + str4 + str4.substring(4)); // tchScritchtch
+
+// 5. Create a new string from a given string by changing the position of first and last characters. The string length must be greater than or equal to 2, i.e., expected output: BoogieWoogie => eoogieWoogiB.
+let str5 = "BoogieWoogie";
+// console.log(str5.replace("B", "e"));
+console.log(str5.replace(str5[0], str5[str5.length - 1])); // eoogieWoogie
+
+// 6. Declare a variable and assign the value "the quick brown fox" (all in lower case). Capitalize the first letter of that string. Print the result to the console.
+let str6 = "the quick brown fox";
+console.log(str6.replace(str6[0], str6[0].toUpperCase(str6[0])));
